13a12cf4e08f55221ed4ac4ee3df41f4c98e0a2a
Roman <rozario91208@gmail.com>
2019-06-29 23:40:29 +0300

    structure project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log(123)
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppHeader from './components/AppHeader';
+import SearchPanel from './components/SearchPanel';
+import TodoList from './components/TodoList';
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
