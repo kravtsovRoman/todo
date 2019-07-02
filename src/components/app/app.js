82eaea1e100d54e33cad0f533f7e5c4b30ee64e9
Roman <rozario91208@gmail.com>
2019-07-02 22:43:18 +0300

    search todos

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,7 +19,8 @@ export default class App extends React.Component {
             this.createTodoItem('Drink coffee'),
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch')
-        ]
+        ],
+        term: ''
     };
 
     createTodoItem(label) {
@@ -89,23 +90,38 @@ export default class App extends React.Component {
         })
     }
 
+    onHandleSearch = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+    }
+
+    search = (items, term) => {
+        if (term.length === 0) return items;
+
+        return items.filter((item) => {
+            return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+        })
+    }
+
     render() {
 
-        const { todoData } = this.state;
+        const { todoData, term } = this.state;
 
         const doneCount = todoData.filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
+
+        const visibleBlock = this.search(todoData, term);
 
         return (
             <div className="todo-app">
                 <AppHeader toDo={todoCount} done={doneCount} />
                 <div className="top-panel d-flex">
-                    <SearchPanel />
+                    <SearchPanel onSearch={this.onHandleSearch} />
                     <ItemStatusFilter />
                 </div>
 
                 <TodoList
-                    todos={todoData}
+                    todos={visibleBlock}
                     onDeleted={this.deleteItem}
                     onToggleImportant={this.onToggleImportant}
                     onToggleDone={this.onToggleDone}
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 import './search-panel.css';
 
-const SearchPanel = () => {
+const SearchPanel = (props) => {
     return (
         <input type="text"
+            onChange={props.onSearch}
             className="form-control search-input"
             placeholder="type to search" />
     );
